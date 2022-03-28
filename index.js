f9071a52f6151f6900025694948443b8fb4bb5f8
Jenni <96969934+jenniwritescode@users.noreply.github.com>
2022-03-28 09:46:35 -0500

    prompts and licensecode generation

--- index.js
+++ index.js
@@ -1,13 +1,80 @@
-// TODO: Include packages needed for this application
+// declare node modules and local modules needed
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        message: "What is your GitHub user name?",
+        name: "gitname",
+        default() {
+          return "gitusername";
+        },
+      },
+      {
+        type: "input",
+        message: "What is your email address?",
+        name: "email",
+        default() {
+          return "gituser@email.com";
+        },
+      },
+      {
+        type: "input",
+        message: "What is the title of your project?",
+        name: "title",
+        default() {
+          return "My Project Title";
+        },
+      },
+      {
+        type: "input",
+        name: "description",
+        message: "Provide a brief description of your project:",
+        default() {
+          return "Project description";
+        },
+      },
+      {
+        type: "input",
+        message: "Provide installation instructions:",
+        name: "install",
+        default() {
+          return "npm i";
+        },
+      },
+      {
+        type: "checkbox",
+        message: "What license should be applied?",
+        name: "license",
+        choices: [ "Apache license 2.0", "Creative Commons license family", "MIT", "Do What The F*ck You Want To Public License" ],
+      },
+];
+
+function askQuestions(questions) {
+    inquirer
+  .prompt(questions)
+  .then((answers) => {
+      const { gitusername, email, title, description, install, license } = answers;
+      console.log(answers);
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      // Prompt couldn't be rendered in the current environment
+    } else {
+      // Something else went wrong
+    }
+  });
+}
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    askQuestions(questions);
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,20 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  let licenseCode = "";
+  if (license == "Apache license 2.0") {
+    licenseCode == "apache-2.0";
+  } else if (license == "Creative Commons license family") {
+    licenseCode == "cc";
+  } else if (license == "MIT") {
+    licenseCode == "mit";
+  } else if (license == "Do What The F*ck You Want To Public License") {
+    licenseCode == "wtfpl";
+  } else {
+    licenseCode = "";
+  }
+  return
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string

d8b87fbd8d8263b32af8e13ac7523cb182e5274d
Jenni <96969934+jenniwritescode@users.noreply.github.com>
2022-04-01 16:52:29 -0500

    working functions

--- index.js
+++ index.js
@@ -1,88 +1,88 @@
 // declare node modules and local modules needed
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generateMarkdown = require('./utils/generateMarkdown.js');
+const generate = require('./utils/generateMarkdown');
 
-const questions = [
+inquirer
+  .prompt([
     {
-        type: "input",
-        message: "What is your GitHub user name?",
-        name: "gitname",
-        default() {
-          return "gitusername";
-        },
+      type: "input",
+      message: "What is your GitHub user name?",
+      name: "gitname",
+      default() {
+        return "gitusername";
       },
-      {
-        type: "input",
-        message: "What is your email address?",
-        name: "email",
-        default() {
-          return "gituser@email.com";
-        },
+    },
+    {
+      type: "input",
+      message: "What is your email address?",
+      name: "email",
+      default() {
+        return "gituser@email.com";
       },
-      {
-        type: "input",
-        message: "What is the title of your project?",
-        name: "title",
-        default() {
-          return "My Project Title";
-        },
+    },
+    {
+      type: "input",
+      message: "What is the title of your project?",
+      name: "title",
+      default() {
+        return "My Project Title";
       },
-      {
-        type: "input",
-        name: "description",
-        message: "Provide a brief description of your project:",
-        default() {
-          return "Project description";
-        },
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Provide a brief description of your project:",
+      default() {
+        return "Project description";
       },
-      {
-        type: "input",
-        message: "Provide installation instructions:",
-        name: "install",
-        default() {
-          return "npm i";
-        },
+    },
+    {
+      type: "input",
+      message: "Provide installation instructions:",
+      name: "install",
+      default() {
+        return "npm i";
       },
-      {
-        type: "checkbox",
-        message: "What license should be applied?",
-        name: "license",
-        choices: [ "Apache license 2.0", "Creative Commons license family", "MIT", "Do What The F*ck You Want To Public License" ],
+    },
+    {
+      type: "input",
+      message: "Provide usage guidelines:",
+      name: "usage",
+      default() {
+        return "non-business use only";
       },
-];
-
-function askQuestions(questions) {
-    inquirer
-  .prompt(questions)
-  .then((answers) => {
-      const { gitusername, email, title, description, install, license } = answers;
-      console.log(answers);
-      return answers;
+    },
+    {
+      type: "input",
+      message: "Provide contribution guidelines:",
+      name: "contribute",
+      default() {
+        return "branch off development and create a pull request";
+      },
+    },
+    {
+      type: "checkbox",
+      message: "What license should be applied?",
+      name: "license",
+      choices: [
+        "Apache license 2.0",
+        "Creative Commons license family",
+        "MIT",
+        "Do What The F*ck You Want To Public License",
+      ],
+    },
+  ])
+  .then(function writeFile(answers) {
+    let markdown = generate(answers);
+    fs.writeFile("myREADME.md", markdown, (err) =>
+      err ? console.log(err) : console.log("success"))
   })
   .catch((error) => {
+    console.log(error);
     if (error.isTtyError) {
       console.log("Prompt couldn't be rendered in the current environment.");
     } else {
       console.log("Something else went wrong.");
     }
   });
-}
-
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-  console.log("writing to file");
-}
-
-// TODO: Create a function to initialize app
-function init() {
-    askQuestions(questions);
-    // writeToFile("myMarkdown.md", answers);
-    // let licenseBadge = generateMarkdown.renderLicenseBadge(answers.license);
-    // let licenseLink = generateMarkdown.renderLicenseLink(answers.license);
-    // let licenseSec = generateMarkdown.renderLicenseSection(answers.license);
-
-}
-
-// Function call to initialize app
-init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,5 +1,57 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
+function generateMarkdown({
+  gitname,
+  email,
+  title,
+  description,
+  install,
+  usage,
+  contribute,
+  license,
+}) {
+  let licenseLink = renderLicenseLink(license);
+  let licenseBadge = renderLicenseBadge(license);
+
+  return `# ${title}
+
+## Table of contents
+
+- [Description](#description)
+- [Installation Instructions](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Contact Info](#contact)
+- [Copyright and license](#copyright-and-license)
+
+## Description
+
+${description}
+
+## Installation Instructions
+
+      ${install}
+
+## Usage
+
+${usage}
+
+## Contributing
+
+${contribute}
+
+## Contact Info
+
+**${gitname}**
+
+- [My GitHub](https://github.com/${gitname})
+- [Email](mailto:${email})
+
+## Copyright and license
+
+Code and documentation: copyright 2022 ${gitname}. 
+Code released under the [${licenseBadge}](${licenseLink}) license.
+`;
+}
+
 function renderLicenseBadge(license) {
   let licenseBadge = "";
   if (license == "Apache license 2.0") {
@@ -13,40 +65,23 @@ function renderLicenseBadge(license) {
   } else {
     licenseBadge = "";
   }
-  console.log(licenseBadge);
   return licenseBadge;
 }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
 function renderLicenseLink(license) {
-  let licenseLink = '';
+  let licenseLink = "";
   if (license == "Apache license 2.0") {
     licenseLink = "https://www.apache.org/licenses/LICENSE-2.0";
-  } else if (license = "Creative Commons license family") {
+  } else if ((license = "Creative Commons license family")) {
     licenseLink = "https://creativecommons.org/licenses/";
-  } else if (license = "MIT") {
+  } else if ((license = "MIT")) {
     licenseLink = "https://opensource.org/licenses/MIT";
   } else if (license == "Do What The F*ck You Want To Public License") {
     licenseLink = "http://www.wtfpl.net/txt/copying/";
   } else {
     licenseLink = "";
   }
-  console.log(licenseLink);
   return licenseLink;
-}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {
-  console.log("creating license section");
-}
-
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
 }
 
 module.exports = generateMarkdown;
